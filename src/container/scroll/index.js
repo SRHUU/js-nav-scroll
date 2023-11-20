7f66d33c3010b15da285cfce9243b25ee3dc163e
SRHUU <sergiychervinko@gmail.com>
2023-11-20 22:54:39 +0200

    nav_scroll_done

---
+++ src/container/scroll/index.js
@@ -0,0 +1,34 @@
+window.buttonScroll.onclick = () =>
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  })
+
+let isDisplay = false
+
+setInterval(() => {
+  if (
+    window.scrollY > window.innerHeight &&
+    isDisplay === false
+  ) {
+    isDisplay = true
+
+    window.buttonScroll.style.display = 'flex'
+
+    // return null
+    return
+  }
+
+  if (
+    window.scrollY <= window.innerHeight &&
+    isDisplay === true
+  ) {
+    isDisplay = false
+
+    window.buttonScroll.style.display = 'none'
+
+    // return null
+    return
+  }
+}, 500)
